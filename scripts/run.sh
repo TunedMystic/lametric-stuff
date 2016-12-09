31964a62a5db9709dfe79cd4c8aa0014856d428b
Sandeep Jadoonanan <jsanweb@gmail.com>
2016-12-09 17:03:52 -0500

    Remove '.pyc' files when starting gunicorn.

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,5 +9,8 @@ cd "$ROOT_PATH"
 # Make log directory.
 mkdir -p "$ROOT_PATH/log"
 
+# Remove '.pyc' files.
+find . -name '*.pyc' -print0|xargs -0 rm
+
 echo 'Starting gunicorn...'
 gunicorn main --bind 0.0.0.0:7000 --access-logfile log/access.log --error-logfile log/error.log

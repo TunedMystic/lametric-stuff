3de1c3c52ab4d03c35bc8b579a7271166cbe0453
Sandeep Jadoonanan <jsanweb@gmail.com>
2016-12-09 13:13:44 -0500

    Fix run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,9 @@
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 
 cd "$PARENT_PATH"
-mkdir -p log
+
+# Make log directory.
+mkdir -p "$PARENT_PATH/log"
 
 echo 'Starting gunicorn...'
 gunicorn main --bind 0.0.0.0:80 --access-logfile log/access.log --error-logfile log/error.log
